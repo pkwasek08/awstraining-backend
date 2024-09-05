4a975bd56a32fa92a1ce5bc075f888f55b32743d
Piotr Kwasek <Piotr.Kwasek@partner.bmw.de>
2024-09-05 14:09:47 +0200

    added lab1 implementation

--- app/src/main/java/com/awstraining/backend/business/notifyme/adapter/MessageSnsAWSSender.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/adapter/MessageSnsAWSSender.java
@@ -1,8 +1,13 @@
 package com.awstraining.backend.business.notifyme.adapter;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
 import com.awstraining.backend.business.notifyme.MessageSender;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,10 +17,15 @@ public class MessageSnsAWSSender implements MessageSender {
 
     // TODO: lab1
     //  1. Inject AWS AmazonsSNS from configuration SNSConfig.
+    private AmazonSNS snsClient;
+    private String snsTopic;
+
     //  2. Make sure that you created new value in parameter store with arn of sns topic.
     //  3. Inject parameter with @Value annotation through constructor.
-//    @Autowired
-    public MessageSnsAWSSender() {
+    @Autowired
+    public MessageSnsAWSSender(@Value("$notification.topicarn") String snsTopic, AmazonSNS snsClient) {
+        this.snsTopic = snsTopic;
+        this.snsClient = snsClient;
     }
 
     @Override
@@ -24,5 +34,8 @@ public class MessageSnsAWSSender implements MessageSender {
         //  1. Create publish request.
         //  2. Publish request with sns.
         //  3. Log information about successful sent message to topic with topic arn and message id.
+        final PublishRequest publishRequest = new PublishRequest(snsTopic, text);
+        final PublishResult publishResult = snsClient.publish(publishRequest);
+        LOGGER.info("Message was send to topic {} with id {}", snsTopic, publishResult.getMessageId());
     }
 }
--- app/src/main/java/com/awstraining/backend/business/notifyme/controller/NotifyMeController.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/controller/NotifyMeController.java
@@ -4,6 +4,8 @@ import com.awstraining.backend.api.rest.v1.NotifyMeApi;
 import com.awstraining.backend.api.rest.v1.model.NotifyMe;
 import com.awstraining.backend.api.rest.v1.model.SentMessage;
 import com.awstraining.backend.business.notifyme.NotifyMeDO;
+import com.awstraining.backend.business.notifyme.NotifyMeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("notification/v1")
 public class NotifyMeController implements NotifyMeApi {
 
+    private final NotifyMeService notifyMeService;
 
-    
     // TODO: lab1
     //  1. Inject service with business logic.
-//    @Autowired
-    public NotifyMeController() {
+    @Autowired
+    public NotifyMeController(NotifyMeService notifyMeService) {
+        this.notifyMeService = notifyMeService;
     }
 
 
@@ -28,7 +31,9 @@ public class NotifyMeController implements NotifyMeApi {
     //  Method should return Http 200 and content of sent message to subscribers.
     @Override
     public ResponseEntity<SentMessage> notifyMe(NotifyMe notifyMe) {
-        return ResponseEntity.notFound().build();
+        final NotifyMeDO notifyMeDO = map(notifyMe);
+        final String message = notifyMeService.notifyMe(notifyMeDO);
+        return ResponseEntity.ok(map(message));
     }
 
     private static SentMessage map(String message) {
